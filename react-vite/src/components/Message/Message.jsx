1164bec7d55b49498076be2094a5d281181ec976
Charles Oller <charlesrello@gmail.com>
2024-06-11 18:56:15 -0400

    messages posting

--- react-vite/src/components/Message/Message.jsx
+++ react-vite/src/components/Message/Message.jsx
@@ -115,3 +115,21 @@
 // };
 
 // export default Message;
+
+import styles from "./Message.module.css"
+import { formatDate } from "./utils/utils"
+
+export default function Message({ text, date, name, img = "https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg"}){
+    return (
+        <article className={styles.message}>
+            <img className={styles.profile_picture} src={img} />
+            <div>
+                <div className={styles.userDetails}>
+                    <h5 className={styles.userName}>{name}</h5>
+                    <p className={styles.date}>{formatDate(date)}</p>
+                </div>
+                <p className={styles.textBody}>{text}</p>
+            </div>
+        </article>
+    )
+}
