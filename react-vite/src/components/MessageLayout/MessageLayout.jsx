a4beb6d9a3fc85e186a9dad156eba35e2bc2f778
garciaa38 <garciaa38@gmail.com>
2024-06-12 21:56:36 -0400

    Moving message component into message layout component

--- react-vite/src/components/MessageLayout/MessageLayout.jsx
+++ react-vite/src/components/MessageLayout/MessageLayout.jsx
@@ -1,10 +1,83 @@
+// import styles from "./MessageLayout.module.css"
+// // Util
+// import { useMemo, useEffect, useState, useRef } from "react"
+// import { io } from "socket.io-client";
+// import { useDispatch, useSelector } from "react-redux";
+// import { createMessageThunk } from "../../redux/message";
+// // import { fetchCurrentUser } from "../../redux/serverUser";
+
+
+// // Components
+// import Message from "../Message/Message"
+// import MessageInput from "../MessageInput/MessageInput"
+
+// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8000';
+// let socket;
+// export default function MessageLayout({ defaultMessages, channelId }) {
+//     const dispatch = useDispatch()
+//     const currentUser = Object.values(useSelector((state) => state.currentUser))[0];
+//     const [messages, setMessages] = useState(defaultMessages)
+    
+//     useEffect(() => {
+//         socket = io(URL);
+//         socket.on('chat', (data) => {
+//             const { text_field: text, user, date } = data;
+//             const newMessage = {
+//                 channel_id: channelId,
+//                 created_at: date,
+//                 updated_at: date,
+//                 message_id: defaultMessages.length + 1,
+//                 text_field: text,
+//                 user
+//             }
+
+//             setMessages(messages => [...messages, newMessage])
+//         })
+
+//         return (() => {
+//             socket.disconnect()
+//         })
+//     }, []);
+            
+//     useEffect(() => {
+//         // socket.emit('leave', { room: prevRoom })
+//         socket.emit('join', { room: channelId })
+//         setMessages(defaultMessages)
+//     }, [channelId])
+
+//     const handleSendMessage = (e, text_field) => {
+//         e.preventDefault()
+//         dispatch(createMessageThunk(channelId, text_field, currentUser.id))
+//         socket.emit('chat', { text_field, room: channelId, user: currentUser, date: new Date() });
+//     }
+
+//     const containerRef = useRef(null);
+                        
+//     useEffect(() => {
+//         // Scroll to the bottom whenever the messages array changes
+//         if (containerRef.current) {
+//             containerRef.current.scrollTop = containerRef.current.scrollHeight;
+//         }
+//     }, [messages]);
+                                
+                                
+//     return (
+//         <div className={styles.main}>
+//             <div className={styles.messages} ref={containerRef}>
+//                 {/* {messageElements} */}
+//                 {/* <button onClick={sendChat}>Click</button> */}
+//             </div>
+//             <MessageInput channelId={channelId} handleSendMessage={handleSendMessage} />
+//         </div>
+//     )
+// }
+
 import styles from "./MessageLayout.module.css"
 // Util
 import { useMemo, useEffect, useState, useRef } from "react"
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
-import { createMessageThunk } from "../../redux/message";
-// import { fetchCurrentUser } from "../../redux/serverUser";
+import { createMessageThunk, updateMessageThunk, deleteMessageThunk } from "../../redux/message";
 
 
 // Components
@@ -13,11 +86,12 @@ import MessageInput from "../MessageInput/MessageInput"
 
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8000';
 let socket;
-export default function MessageLayout({ defaultMessages, channelId }) {
+export default function MessageLayout({ defaultMessages, channelId, prevChannelId }) {
     const dispatch = useDispatch()
     const currentUser = Object.values(useSelector((state) => state.currentUser))[0];
-    const [messages, setMessages] = useState(defaultMessages)
-    
+    const [messages, setMessages] = useState([])
+
+
     useEffect(() => {
         socket = io(URL);
         socket.on('chat', (data) => {
@@ -38,9 +112,9 @@ export default function MessageLayout({ defaultMessages, channelId }) {
             socket.disconnect()
         })
     }, []);
-            
+
     useEffect(() => {
-        // socket.emit('leave', { room: prevRoom })
+        socket.emit('leave', { room: prevChannelId })
         socket.emit('join', { room: channelId })
         setMessages(defaultMessages)
     }, [channelId])
@@ -48,23 +122,38 @@ export default function MessageLayout({ defaultMessages, channelId }) {
     const handleSendMessage = (e, text_field) => {
         e.preventDefault()
         dispatch(createMessageThunk(channelId, text_field, currentUser.id))
+        console.log("CURRENT USER: ", currentUser)
         socket.emit('chat', { text_field, room: channelId, user: currentUser, date: new Date() });
     }
 
+    const handleEditMessage = (messageId, newText) => {
+        dispatch(updateMessageThunk(messageId, newText, channelId));
+    }
+
+    const handleDeleteMessage = (messageId, reaction) => {
+        dispatch(deleteMessageThunk(messageId, channelId));
+    }
+
+    const messageElements = useMemo(() => messages.map((message) => {
+        const { user } = message;
+        const url = user?.profile_images[0]?.url || undefined
+        return <Message key={message.id} message={message} text={message.text_field} date={message.updated_at} name={message.user?.username} img={url} onEdit={handleEditMessage} onDelete={handleDeleteMessage} />
+    }), [messages])
+
     const containerRef = useRef(null);
-                        
+
     useEffect(() => {
         // Scroll to the bottom whenever the messages array changes
         if (containerRef.current) {
             containerRef.current.scrollTop = containerRef.current.scrollHeight;
         }
     }, [messages]);
-                                
-                                
+
+
     return (
         <div className={styles.main}>
             <div className={styles.messages} ref={containerRef}>
-                {/* {messageElements} */}
+                {messageElements}
                 {/* <button onClick={sendChat}>Click</button> */}
             </div>
             <MessageInput channelId={channelId} handleSendMessage={handleSendMessage} />
