873c12706722537e83ae508aa49c7732d56c8c7e
Charles Oller <charlesrello@gmail.com>
2024-06-13 21:04:12 -0400

    modified migration

--- migrations/versions/140c049b264a_initial_migration.py
+++ migrations/versions/140c049b264a_initial_migration.py
@@ -8,6 +8,10 @@ Create Date: 2024-06-13 20:48:49.411899
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = '140c049b264a'
